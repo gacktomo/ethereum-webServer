6bb753fd1ce3012549cfa53cedb2b9a6b68df4fd
Tomohiro Matsuo <gacktomo@gmail.com>
2017-01-15 21:01:45 +0900

    strings convert to bytes32

--- webServer.sol
+++ webServer.sol
@@ -4,18 +4,26 @@ contract webServer {
     address owner;
     string data;
   }
-  pageData firstPage;
 
-  event RegisterEvent(address owner, string data);
+  mapping(bytes32 => pageData) public pages;
+  //event RegisterEvent(address owner, string data);
 
-  function webServer(string data) {
+  function set(string name, string data) {
     //if (domainList[domain].used==true) return false;
-    firstPage.owner = msg.sender;
-    firstPage.data = data;
-    RegisterEvent(msg.sender, data);
+    var name32 = stringToBytes32(name);
+    pages[name32].owner = msg.sender;
+    pages[name32].data = data;
+    //RegisterEvent(msg.sender, data);
   }
 
-  function get() constant returns (string data){
-    return firstPage.data;
+  function get(string name) constant returns (string data){
+    var name32 = stringToBytes32(name);
+    return pages[name32].data;
+  }
+
+  function stringToBytes32(string memory source) returns (bytes32 result) {
+    assembly {
+        result := mload(add(source, 32))
+    }
   }
 }
