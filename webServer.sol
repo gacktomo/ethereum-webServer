4a07c95427c7d45985959ffcdf11f573999a444a
Tomohiro Matsuo <gacktomo@gmail.com>
2017-01-19 20:34:45 +0900

    data type change to array

--- webServer.sol
+++ webServer.sol
@@ -2,25 +2,28 @@ pragma solidity ^0.4.0;
 contract webServer {
   struct pageData{
     address owner;
-    string data;
+    bytes32[] data;
   }
 
   mapping(bytes32 => mapping(bytes32 => pageData)) public pages;
   //event RegisterEvent(address owner, string data);
 
-  function set(bytes32 domain, bytes32 name, string data) returns (bool result){
+  function set(bytes32 domain, bytes32 name, bytes32 data) returns (bool result){
     if (pages[domain][name].owner != 0) {
         if (pages[domain][name].owner != msg.sender) {
             return false;
         }
     }
     pages[domain][name].owner = msg.sender;
-    pages[domain][name].data = data;
+    pages[domain][name].data.push(data);
     return true;
     //RegisterEvent(msg.sender, data);
   }
 
-  function get(bytes32 domain, bytes32 name) constant returns (string data){
+  function get(bytes32 domain, bytes32 name) constant returns (bytes32[] data){
     return pages[domain][name].data;
+  }
+  function getlen(bytes32 domain, bytes32 name) constant returns (uint leng){
+    return pages[domain][name].data.length;
   }
 }
