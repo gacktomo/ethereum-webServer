338c81d018db73d77f86cd5469790dba130181fc
Tomohiro Matsuo <gacktomo@gmail.com>
2017-01-20 05:01:42 +0900

    change data get method

--- webServer.sol
+++ webServer.sol
@@ -8,14 +8,14 @@ contract webServer {
   mapping(bytes32 => mapping(bytes32 => pageData)) public pages;
   //event RegisterEvent(address owner, string data);
 
-  function set(bytes32 domain, bytes32 name, bytes data) returns (bool result){
+  function set(bytes32 domain, bytes32 name) returns (bool result){
     if (pages[domain][name].owner != 0) {
         if (pages[domain][name].owner != msg.sender) {
             return false;
         }
     }
     pages[domain][name].owner = msg.sender;
-    pages[domain][name].data=data;
+    pages[domain][name].data=msg.data;
     return true;
     //RegisterEvent(msg.sender, data);
   }
