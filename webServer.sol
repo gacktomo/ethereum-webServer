931660bde872cfc613e705d42bb6db7bdbfbe2f4
Tomohiro Matsuo <gacktomo@gmail.com>
2017-01-20 18:55:45 +0900

    add timestamp arg

--- webServer.sol
+++ webServer.sol
@@ -2,18 +2,23 @@ pragma solidity ^0.4.0;
 contract webServer {
   struct pageData{
     address owner;
+    bytes32 timestamp;
     bytes32[] data;
   }
 
   mapping(bytes32 => mapping(bytes32 => pageData)) public pages;
   event RegisterEvent(address owner, bytes32 data);
 
-  function set(bytes32 domain, bytes32 name, bytes32 data) returns (bool result){
+  function set(bytes32 time, bytes32 domain, bytes32 name, bytes32 data) returns (bool result){
     if (pages[domain][name].owner != 0) {
         if (pages[domain][name].owner != msg.sender) {
             return false;
         }
     }
+    if(pages[domain][name].timestamp!=time){
+        deletePage(domain,name);
+    }
+    pages[domain][name].timestamp = time;
     pages[domain][name].owner = msg.sender;
     pages[domain][name].data.push(data);
     RegisterEvent(msg.sender, data);
@@ -23,6 +28,11 @@ contract webServer {
 
   function get(bytes32 domain, bytes32 name) constant returns (bytes32[] data){
     return pages[domain][name].data;
+  }
+
+  function deletePage(bytes32 domain, bytes32 name) returns (bool result){
+    pages[domain][name].data.length=0;
+    return true;
   }
 
   function getlen(bytes32 domain, bytes32 name) constant returns (uint leng){
